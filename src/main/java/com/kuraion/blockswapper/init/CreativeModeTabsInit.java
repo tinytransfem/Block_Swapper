2e2e2cb003bbe7bf364ea69504a343f9ec203eb6
tinytransfem <n3rilp@gmail.com>
2023-09-18 14:57:46 +0200

    Updated Creative Mode Tab Code to 1.20.1

--- src/main/java/com/kuraion/blockswapper/init/CreativeModeTabsInit.java
+++ src/main/java/com/kuraion/blockswapper/init/CreativeModeTabsInit.java
@@ -1,15 +1,15 @@
 package com.kuraion.blockswapper.init;
 
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CreativeModeTabsInit {
     @SubscribeEvent
-    public static void addItems(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+    public static void addItems(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(ItemsInit.COPPER_BLOCK_SWAPPER);
             event.accept(ItemsInit.NETHERITE_BLOCK_SWAPPER);
         }
